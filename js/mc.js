3eb3b7415ab89d938a31156dce1215ef61d156d0
Laura Henze <laurahenze@gmail.com>
2014-04-18 18:01:46 -0400

    Removed all global nav JS from the main JS file
    It is now in the global nav module

--- js/mc.js
+++ js/mc.js
@@ -1,126 +1,7 @@
 (function($) {
-
-    function respond() {
-        var w = $(window).width();
-        // remove inline display styles set by jquery.
-        $("header[role=banner] *").removeAttr("style");
-        closeLoginLink();
-        if (w < 600) {
-            setUpMobileView();
-        } else {
-            setUpTabletView();
-        }
-    }
-
-    function setUpMobileView() {
-        $("a.logolink").unbind("click");
-        $("a.drop-down").remove();
-        $(".navitems2 ul").children().unwrap().unwrap();
-        $("a.logolink").click(function(e) {
-            $('nav.global li.sites_nav ul').toggleClass("open", 100);
-            e.preventDefault();
-            e.stopPropagation();
-        });
-        $("a.logolink").blur(function() {
-            $('ul.navitems').toggleClass("open", false, 100);
-
-        });
-        $(document).click(function(e) {
-            e.preventDefault();
-            $('nav.global li.sites_nav ul').toggleClass("open", false, 100);
-            // Everything with an "open" class should be closed here 
-        });
-    }
-
-    function setUpTabletView() {
-        $("a.logolink").unbind();
-        $(document).unbind("click");
-        $("a.drop-down").remove();
-        $(".navitems2 ul").children().unwrap().unwrap();
-        var widthSum = 0;
-        var spaceWidth = ($("nav.global").width() - $("a.logolink.mc-logo").width() - $("li.utils").width()) - 125;
-        //console.log('$("nav.global").width() ' + $("nav.global").width() + ' ||| a.logolink.mc-logo ' + $("a.logolink.mc-logo").width() + ' ||| $("li.utils").width()' + $("li.utils").width());
-        $("nav.global li.sites_nav li").each(function(index) {
-            widthSum += $(this).width();
-            if (widthSum > spaceWidth) {
-                $(this).nextAll('li').addBack().wrapAll("<li class='navitems2' role='menuitem'><ul class='more-items'></ul><li>");
-                $('.navitems2').prepend('<a class="drop-down" href="#">More</a>');
-                $("a.drop-down").click(function() {
-                    $('.navitems2').toggleClass("open");
-                    $('.navitems').toggleClass("open");
-                    $(this).toggleClass("open");
-
-                });
-                return false;
-            }
-        });
-    }
-
-    function closeUpShop() {
-        // for everything that has the open class, remove it
-        $('.open').removeClass("open");
-        $('[aria-hidden="false"]').attr('aria-hidden', 'true');
-    }
-
-    function siteHeaderBehavior() {
-        // SEARCH
-        $('header[role=banner] form[role=search] label').click(function() {
-            $theSearchButton = $(this);
-            if ($($theSearchButton).parent('form').hasClass("open")) {
-                // $(this).parent('form').find('fieldset').attr('aria-hidden', 'true');
-                //$(this).parent('form').toggleClass("open", 400);
-                $(this).parent('form').removeClass("open");
-                $($theSearchButton).parent('form').find('fieldset').slideUp(300,
-                    function() {
-                        closeUpShop();
-                        $(this).parent('form').removeClass("open");
-                    });
-            } else {
-                // $(this).parent('form').find('fieldset').attr('aria-hidden', 'false');
-                //$(this).parent('form').toggleClass("open", 400);
-                $('nav.main ul').slideUp(300, function() {
-                    $('header[role=banner] button').removeClass("open");
-                    $('header[role=banner] form[role=search] fieldset').slideDown(300,
-                        function() {
-                            $(this).parent('form').addClass("open");
-                        });
-                });
-            }
-        });
-        $("header[role=banner] button").click(function() {
-            var $theMenuButton = this;
-            if ($("nav.main ul").is(":visible")) {
-                //console.log(" is visible");
-                $('nav.main ul').slideUp(300, function() {
-                    $($theMenuButton).removeClass("open");
-                });
-            } else {
-                console.log(" is NOT visible");
-                $('header[role=banner] form[role=search] fieldset').slideUp(200,
-                    function() {
-                        $('nav.main ul').slideDown(300, function() {
-                            $('header[role=banner] form[role=search]').removeClass("open");
-                            $($theMenuButton).addClass("open");
-                        });
-                    });
-            }
-        });
-    }
-
-    function setUpLoginLink() {
-        // Global nav - Login - global behavior
-        $('.utils a.login-link').click(function() {
-            // $('.utils ul[role="menu"]').show(1000).attr('aria-hidden', 'false');
-            if ($(this).next('ul').is(":visible")) {
-                closeLoginLink();
-            } else {
-                openLoginLink();
-            }
-        });
-    }
-
     function setUpAsideReveal() {
         // visibility of arrow determined by CSS media queries
+        console.log("setUpAsideReveal");
         if ($('aside[role="complimentary"].part-of-hub .responsive-disclosure').is(":visible")) {
             $('aside[role="complimentary"].part-of-hub header').click(function() {
                 if ($(this).next('nav').is(":hidden")) {
@@ -141,47 +22,12 @@
             $('aside[role="complimentary"].part-of-hub header').unbind("click");
             $('aside[role="complimentary"].part-of-hub').removeClass('open');
         }
-
-    }
-
-    function closeLoginLink(e) {
-        // if (!e) {
-        //     e = window.event;
-        // }
-        //e.preventDefault();
-        $('.utils ul').slideUp(100,
-            function() {
-                $('.utils a.login-link').removeClass("open");
-                $('.utils ul[aria-hidden="false"]').attr('aria-hidden', 'true');
-            });
-    }
-
-    function openLoginLink(e) {
-        if (!e) {
-            e = window.event;
-        }
-        e.preventDefault();
-        $('.utils a.login-link').addClass("open");
-        $('.utils ul').slideDown(100,
-            function() {
-                $('.utils ul[aria-hidden="true"]').attr('aria-hidden', 'false');
-
-            });
     }
     jQuery(document).ready(function($) {
-        respond();
-        siteHeaderBehavior();
-        setUpLoginLink();
         setUpAsideReveal();
         $(window).resize(_.debounce(function() {
-            console.log("resized");
             setUpAsideReveal();
-            respond();
         }, 100));
     });
-    $(window).bind("load", function() {
-        //nav resize code here
-        respond();
 
-    });
 })(jQuery);

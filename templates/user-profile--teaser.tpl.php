3234a0427d21bf37301e1a6da6ca21edf2fc9ef0
Laura Henze <laurahenze@gmail.com>
2014-06-25 14:08:58 -0400

    not used

--- templates/user-profile--teaser.tpl.php
+++
@@ -1,9 +0,0 @@
-<section class="user-teaser">Tease!
- 
-  
-  <div class="user-body">
-    <?php print render($user_profile['field_location']); ?>
-    <?php print render($user_profile['field_organization']); ?>
-  
-  </div> 
-</section>

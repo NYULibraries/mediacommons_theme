ad799a93e13178f4eeef70f3e65197f37e42e984
Laura Henze <laurahenze@gmail.com>
2014-07-29 13:02:29 -0600

    Layout templates for Display Suite 2 column

---
+++ templates/ds-2col-stacked.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+
+<?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+<?php endif; ?>
+
+<?php print "<header>"; ?>
+  <?php print $header; ?>
+<?php print "</header>"; ?>
+
+<<?php print $left_wrapper ?> class="<?php print $left_classes; ?>">
+  <?php print $left; ?>
+</<?php print $left_wrapper ?>>
+
+<?php print $right; ?>
+
+
+  <<?php print $footer_wrapper ?> class="<?php print $footer_classes; ?>">
+    <?php print $footer; ?>
+  </<?php print $footer_wrapper ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>

419a2656e1838b46c48674634330333984adc4dd
Laura Henze <laurahenze@gmail.com>
2014-08-08 16:04:43 -0400

    Tidying

--- userupdaterule.php
+++ userupdaterule.php
@@ -1,23 +1,23 @@
 <?php
 
 $uids = db_query('SELECT uid FROM {users}  WHERE uid < 1000 ORDER BY uid DESC LIMIT 1000 ')->fetchCol();
-  echo "\n ";   
+echo "\n ";   
 $users = user_load_multiple($uids);
 foreach ($users as $user) {
-unset($p);
-unset($profile);
-  echo "\n "; 
+	unset($p);
+	unset($profile);
+  	echo "\n "; 
     $p = profile2_load_by_user($user, $type_name = 'mediacommonsprofile');
- if (empty( $p) ) {
-      echo "p is empty "; 
-echo "no profile " . $user->name;
- $profile = profile2_create(array('type' => 'mediacommonsprofile', 'uid' => $user->uid));
-$profile->field_full_name['und'][0]['value'] = $user->name;
-$profile->save();
-echo "just saved  ";
-} else {
-echo "yes has profile " . $user->name;
-}
+ 	if (empty( $p) ) {
+		echo "p is empty "; 
+		echo "no profile " . $user->name;
+		$profile = profile2_create(array('type' => 'mediacommonsprofile', 'uid' => $user->uid));
+		$profile->field_full_name['und'][0]['value'] = $user->name;
+		$profile->save();
+		echo "just saved  ";
+	} else {
+	echo "yes has profile " . $user->name;
+	}
 }
 
 ?>

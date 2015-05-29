f1ff278c5162295d9c2e63462458c20d0995a1d9
Laura Henze <laurahenze@gmail.com>
2015-05-29 16:33:34 -0400

    Sidebars are now set up on page load, not ready

--- js/mc.js
+++ js/mc.js
@@ -1,34 +1,41 @@
+;
 (function($) {
-    function setUpAsideReveal() {
+  Drupal.behaviors.mediacommons_responsivesidebars = {
+    attach: function(context, settings) {
+      function setUpAsideReveal() {
         // visibility of arrow determined by CSS media queries
         console.log('setUp sidebars');
         if ($('aside[role="complimentary"].part-of-hub .responsive-disclosure').is(':visible')) {
-            $('aside[role="complimentary"].part-of-hub header').click(function() {
-                if ($(this).next('nav').is(':hidden')) {
-                    $(this).next('nav').slideDown(300,
-                        function() {
-                            // open class is for the arrow display
-                            $(this).parent('aside').addClass('open');
-                        });
-                } else {
-                    $(this).next('nav').slideUp(300,
-                        function() {
-                            $(this).parent('aside').removeClass('open');
-                        });
-                }
-            });
+          $('aside[role="complimentary"].part-of-hub header').click(function() {
+            if ($(this).next('nav').is(':hidden')) {
+              $(this).next('nav').slideDown(300,
+                function() {
+                  // open class is for the arrow display
+                  $(this).parent('aside').addClass('open');
+                });
+            } else {
+              $(this).next('nav').slideUp(300,
+                function() {
+                  $(this).parent('aside').removeClass('open');
+                });
+            }
+          });
         } else {
-            $("aside[role='complimentary'].part-of-hub nav").removeAttr('style');
-            $('aside[role="complimentary"].part-of-hub header').unbind('click');
-            $('aside[role="complimentary"].part-of-hub').removeClass('open');
+          $("aside[role='complimentary'].part-of-hub nav").removeAttr('style');
+          $('aside[role="complimentary"].part-of-hub header').unbind('click');
+          $('aside[role="complimentary"].part-of-hub').removeClass('open');
         }
-    }
-    jQuery(document).ready(function($) {
+      }
+      $(window).resize(_.debounce(function() {
+        // executed when the DOM is ready
+        console.log("resized (debounced) from from mc.js");
         setUpAsideReveal();
-        $(window).resize(_.debounce(function() {
-            setUpAsideReveal();
-        }, 100));
-    });
-
+      }, 100));
+      $(window).bind("load", function() {
+        // executed once every element of the page is loaded, including fonts
+        console.log("Load event triggered from mc.js");
+        setUpAsideReveal();
+      });
+    }
+  }
 })(jQuery);
-

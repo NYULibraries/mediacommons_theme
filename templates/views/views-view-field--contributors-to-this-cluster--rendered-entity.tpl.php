91d68c1298f8dd07eb07ee86431d6db5a0f6ff98
Laura Henze <laurahenze@gmail.com>
2015-08-04 17:15:01 -0400

    templates for "contributors" sidebars

---
+++ templates/views/views-view-field--contributors-to-this-cluster--rendered-entity.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<div class="h-card vcard">
+    <?php print $output; ?>
+</div>
+

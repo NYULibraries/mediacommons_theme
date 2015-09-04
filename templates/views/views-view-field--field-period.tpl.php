053ebb1fe273c642134146d05a0c15b0b7275883
Laura Henze <laurahenze@gmail.com>
2015-09-04 16:13:07 -0400

    Theming additions for IMR homepage's "featured" section

---
+++ templates/views/views-view-field--field-period.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Simple view template to output only markup 
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php  if ($output) : ?>
+<time>
+<?php 
+print $output;
+?></time>
+<?php endif; ?>
+

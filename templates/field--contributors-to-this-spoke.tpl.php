8aca6584c95aa7c11faed11b0271aaf290c0abd6
Laura Henze <laurahenze@gmail.com>
2015-11-16 17:17:27 -0500

    Adding layout for comments on user page

---
+++ templates/field--contributors-to-this-spoke.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Simple Field Posts from User.
+ *
+ * This file is not used and is here as a starting point for customization only.
+ * @see theme_field()
+ *
+ *
+ * @see template_preprocess_field()
+ * @see theme_field()
+ *
+ * @ingroup themeable
+ */
+?>
+
+<div class="peoplelist clearfix <?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="label">By:</div> 
+ 	<?php foreach ($items as $delta => $item): ?>
+      <?php print render($item); ?>
+ 	<?php endforeach; ?>
+</div>

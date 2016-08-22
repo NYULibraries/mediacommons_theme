60cbece83270c2e51d2aa3e834f968791a860a21
Laura Henze <laura.henze@nyu.edu>
2016-08-22 16:48:12 -0400

    microformats: h-card for spoke page bylines

---
+++ templates/ds-1col--user-user-byline.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+<<?php print "div"; print $layout_attributes; ?> class="h-card vcard <?php print $classes;?>">
+<?php print $ds_content; ?>
+</div>

a8efa3cfb0e0e2bf5ef3be44ec994effc3c788a9
Laura Henze <laura.henze@nyu.edu>
2016-08-23 14:58:07 -0400

    User view modes: teaser needs clear fix; byline does not

--- templates/ds-1col--user-user-teaser.tpl.php
+++ templates/ds-1col--user-user-teaser.tpl.php
@@ -5,6 +5,6 @@
  * Display Suite 1 column template.
  */
 ?>
-<<?php print "div"; print $layout_attributes; ?> class="ds-1col h-card vcard <?php print $classes;?>">
+<<?php print "div"; print $layout_attributes; ?> class="ds-1col h-card vcard clearfix <?php print $classes;?>">
 <?php print $ds_content; ?>
 </div>

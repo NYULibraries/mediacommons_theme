3970e7eddac8af6859ee2a83ada89f221e4cb54d
Laura Henze <laura.henze@nyu.edu>
2016-08-31 15:08:10 -0400

    Comments preprocess hook: if no org, do not give that var value

--- template.php
+++ template.php
@@ -269,14 +269,14 @@ function mediacommons_preprocess_comment(&$variables) {
   $user = user_load($comment->uid);
   // Get Organization of the commenter, and send that variable on to the template
   $orgA = field_get_items('user', $user, 'field_organization');
-  if ($orgA) {$tid = $orgA[0]['tid']; }
+  if ($orgA) {$tid = $orgA[0]['tid']; 
   $term = taxonomy_term_load($tid); // load term object
   $term_uri = taxonomy_term_uri($term); // get array with path
   $term_title = taxonomy_term_title($term);
   $term_path = $term_uri['path'];
   $link = l($term_title,$term_path);
   $variables['organization'] = $link;
-
+  }
 
 
   $variables['created'] = format_date($comment->created, 'custom', 'l, F j, Y —  g:i a');

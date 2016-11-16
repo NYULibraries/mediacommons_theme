ea42ea4fdf28d0bb43c1c2b414ecc454b8d3edcf
Laura Henze <laura.henze@nyu.edu>
2016-11-16 13:12:37 -0500

    Simplified Template: Contributed by header

---
+++ templates/field--posts-contributed--user.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Simple Field Posts from User.
+ *
+ * This file is not used and is here as a starting point for customization only.
+ * @see theme_field()
+ *
+ *
+ * @see template_preprocess_field()
+ * @see theme_field()
+ *
+ * @ingroup themeable
+ */
+?>
+
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+ <?php foreach ($items as $delta => $item): ?>
+      <?php print render($item); ?>
+    <?php endforeach; ?>
+</div>

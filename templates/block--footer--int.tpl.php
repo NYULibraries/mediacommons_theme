ab28e60f81875f5dbea80bc9bf06c10811c167d1
Laura Henze <laura.henze@nyu.edu>
2016-11-16 12:36:30 -0500

    InTransition Footer: Styles for the SCMS image

--- templates/block--footer--int.tpl.php
+++ templates/block--footer--int.tpl.php
@@ -8,7 +8,8 @@
 <?php 
 $mclink = $GLOBALS['base_root'] . "/mediacommons" ; 
 ?>
-
-<p class="extras"><strong>[in]Transition:</strong> A <a href="<?php echo $mclink; ?>">MediaCommons</a>/<a href="http://www.cmstudies.org/?page=cinema_journal">Cinema Journal</a> Project |  <a href="http://www.cmstudies.org/" target="_blank"><img alt="SCMS" src="http://mediacommons.futureofthebook.org/intransition/sites/devmc.dlib.nyu.edu.intransition/themes/in_transition/images/scms_logo_sm.gif" ></a>&nbsp; |&nbsp;<a href="/intransition/copyright">Copyright/Fair Use Policy</a></p>
+<div class= "<?php print $classes ?>""  >
+<p class="extras"><strong>[in]Transition:</strong> A <a href="<?php echo $mclink; ?>">MediaCommons</a>/<a href="http://www.cmstudies.org/?page=cinema_journal">Cinema Journal</a> Project |  <a href="http://www.cmstudies.org/" target="_blank" title="Society For Cinema and Media Studies" style="line-height: 1;font-size: .1rem; display: inline-block; vertical-align: bottom;"><img alt="Society For Cinema and Media Studies" src="http://mediacommons.futureofthebook.org/intransition/sites/devmc.dlib.nyu.edu.intransition/themes/in_transition/images/scms_logo_sm.gif" ></a>&nbsp; |&nbsp;<a href="/intransition/copyright">Copyright/Fair Use Policy</a></p>
 
 <?php print $content; ?>
+</div>

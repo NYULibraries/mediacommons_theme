af0a0d0852e8d58b59af704cc5db633c622af4ab
Laura Henze <laura.henze@nyu.edu>
2017-04-21 11:53:22 -0400

    Documentation

--- templates/ds-1col--user-user-teaser.tpl.php
+++ templates/ds-1col--user-user-teaser.tpl.php
@@ -3,6 +3,7 @@
 /**
  * @file
  * Display Suite 1 column template.
+ * LMH 21-04-17 This adds the microformats classes
  */
 ?>
 <<?php print "div"; print $layout_attributes; ?> class="ds-1col h-card vcard clearfix <?php print $classes;?>">

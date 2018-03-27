f68f2cdef4cb608fcadb5ea6c0f6f48594da90dd
Laura Henze <laura.henze@nyu.edu>
2018-03-27 19:29:53 -0400

    Footer templates

--- templates/block--footer.tpl.php
+++ templates/block--footer.tpl.php
@@ -1,1 +1,7 @@
+<?php $mclink = $GLOBALS['base_root'] ;  ?>
+<p class="extras">
+	<span><a href="<?php echo $mclink; ?>/about">About MediaCommons</a></span>
+	<span><a href="<?php echo $mclink; ?>/editors">Editors</a></span>
+	<span><a href="<?php echo $mclink; ?>/copyright">Copyright/Fair Use Policy</a></span>
+</p>
 <?php print $content; ?>

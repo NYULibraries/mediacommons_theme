45a2b0a5ecb0d7be01a429416c426f8eb78adcc9
Laura Henze <laura.henze@nyu.edu>
2018-03-28 13:20:30 -0400

    New placeholder text; responsive text styles

--- templates/page--front--mc.tpl.php
+++ templates/page--front--mc.tpl.php
@@ -1,45 +1,63 @@
 <a id="main-content"></a>
-<header  role="banner"> 
-  <h1><a href="<?php print $front_page; ?>" title="Home" rel="home" class="logo"><span><?php print $site_name; ?></span></a></h1>
+<header role="banner">
+  <h1>
+    <a href="<?php print $front_page; ?>" title="Home" rel="home" class="logo">
+      <span>
+        <?php print $site_name; ?>
+      </span>
+    </a>
+  </h1>
   <?php print render($page['search']); ?>
 </header>
 <?php if ($action_links): ?>
-  <ul class="action-links">
-    <?php print render($action_links); ?>
-  </ul>
+<ul class="action-links">
+  <?php print render($action_links); ?>
+</ul>
 <?php endif; ?>
 <main role="main">
   <?php if (isset($tabs)): ?>
-    <div class="tabs"><?php print render($tabs); ?></div>
+  <div class="tabs">
+    <?php print render($tabs); ?>
+  </div>
   <?php endif; ?>
   <?php if (isset($page['highlighted'])) : ?>
-    <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+  <div id="highlighted">
+    <?php print render($page['highlighted']); ?>
+  </div>
   <?php endif; ?>
   <?php if (isset($messages)) : ?>
-    <?php print $messages; ?>
+  <?php print $messages; ?>
   <?php endif; ?>
-  <div id="content"> 
+  <div id="content">
     <div class="flexhold">
-      <section class="featured-more">  
+      <section class="featured-more">
         <div class="hometophold">
-          <div class="hometopleft"><?php  print render($page['umb_homepage_top']);  ?></div>
-          <div class="hometopright">
-            <p><span class="special">Placeholder!  Welcome</span> Fermentum posuere parturient egestas suscipit mi mi ad faucibus viverra habitasse dictumst vestibulum convallis condimentum parturient.Per ac euismod varius per elit eget at parturient nibh laoreet leo vulputate a mi vestibulum ullamcorper justo consectetur ad a nec.</p>
-            <p><a href="#">Elit mi justo volutpat</a></p>
+          <div class="hometopleft">
+            <?php  print render($page['umb_homepage_top']);  ?>
+          </div>
+          <div class="hometopright node-mc">
+            <div class="special">Welcome<span> to MediaCommons</span>!</div>
+            <div class="maintext">
+              We are a community network for scholars, students, and practitioners in media studies, promoting exploration of new forms of publishing within the field.
+            </div>
+            <div>
+              <a href="/about">Read more...</a>
+            </div>
           </div>
         </div>
-        <?php  print render($page['umb_homepage_bottom']); ?> 
+        <?php  print render($page['umb_homepage_bottom']); ?>
       </section>
       <aside role="complementary">
         <?php print render($page['umb_homepage_sidebar']); ?>
       </aside>
-     </div>
     </div>
-  </main>
-  <?php if (isset($page['globalnav']) && !empty($page['globalnav'])) : ?>
-    <nav class="global" role="navigation"><?php print render($page['globalnav']); ?></nav>
-  <?php endif; ?>
-  <?php if (isset($page['footer']) && !empty($page['footer'])) : ?>
-    <?php print render($page['footer']); ?> 
-  <?php endif; ?>
- 
+  </div>
+</main>
+<?php if (isset($page['globalnav']) && !empty($page['globalnav'])) : ?>
+<nav class="global" role="navigation">
+  <?php print render($page['globalnav']); ?>
+</nav>
+<?php endif; ?>
+<?php if (isset($page['footer']) && !empty($page['footer'])) : ?>
+<?php print render($page['footer']); ?>
+<?php endif; ?>

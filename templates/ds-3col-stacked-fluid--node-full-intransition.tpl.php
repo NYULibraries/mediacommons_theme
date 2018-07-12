1155a388870fa2b5335446b6f2bdd5a5d2628577
Laura Henze <laura.henze@nyu.edu>
2018-07-12 12:08:42 -0400

    "Back to issue" link

--- templates/ds-3col-stacked-fluid--node-full-intransition.tpl.php
+++ templates/ds-3col-stacked-fluid--node-full-intransition.tpl.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
- * @file
- * Display Suite fluid 3 column 25/50/25 stacked template.
- */
+  * MediaCommons --
+  * Spoke page for [in]Transition
+  *
+  * Display Suite fluid 3 column 25/50/25 stacked template.
+*/
 
   // Add sidebar classes so that we can apply the correct width to the center region in css.
   if (($left && !$right) || ($right && !$left)) $classes .= ' group-one-sidebar';
@@ -16,7 +18,17 @@
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
-  <nav class="backToIssue"><a href="/intransition/theme-week/2018/10/journal-videographic-film-mloving-image-studies-51-2018">Journal of Videographic Film &amp; Moving Image Studies, 5.1, 2018</a></nav>
+
+
+  <?php
+  $hub_id = $field_part_of["und"][0]['nid']; 
+  $hub_node = node_load($hub_id);
+  $hub_title = $hub_node->title;
+  $hub_url = url( 'node/' . $hub_id ); 
+?>
+
+  <nav class="backToIssue"><a href="<?php print  $hub_url ?>"><?php print  $hub_title ?></a></nav>
+
    <header class="group-header <?php print $header_classes; ?>">
     <?php print $header; ?>
   </header>

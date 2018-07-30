f3226450186c3e0bf5bdeba294b24404896db3ae
Laura Henze <laura.henze@nyu.edu>
2018-07-30 12:38:41 -0400

    Accessibility updates

--- templates/components/apachesolrsortlist.tpl.php
+++ templates/components/apachesolrsortlist.tpl.php
@@ -1,6 +1,6 @@
 <div class="select-hold">
   <div class="select-style">
-    <select name="searchresults-sort" id="searchresults-sort" class="field-select">
+    <select name="searchresults-sort" id="searchresults-sort" class="field-select" aria-label="Sort results">
       <?php foreach ($items as $item) : ?>
         <?php print $item ?>
       <?php endforeach ?>

c00c9fead0d782c49a81bcbf7c851d155a9b25a3
Laura Henze <laura.henze@nyu.edu>
2018-09-12 17:49:28 -0400

    Flexbox and ULs for footer lists

--- templates/block--footer--int.tpl.php
+++ templates/block--footer--int.tpl.php
@@ -7,15 +7,14 @@ $main_menu = menu_navigation_links('main-menu');
   <?php print theme('links__system_main_menu', array('links' => $main_menu )); ?>
   <?php endif; ?>
 </nav>
-<div class="<?php print $classes ?>">
-  <p class="extras">
+<div class="footer-nav">
+  <ul><li>
     <strong>[in]Transition:</strong> A
     <a href="<?php echo $mclink; ?>">MediaCommons</a>/
-    <a href="http://www.cmstudies.org/?page=cinema_journal">Cinema Journal</a> Project |
+    <a href="http://www.cmstudies.org/?page=cinema_journal">Cinema Journal</a> Project</li><li>
     <a href="http://www.cmstudies.org/" target="_blank" title="Society For Cinema and Media Studies" style="line-height: 1;font-size: .1rem; display: inline-block; vertical-align: bottom;">
       <img alt="Society For Cinema and Media Studies" src="<?php echo $theme_path;?>/images/logos/scms_logo_sm.gif">
-    </a>&nbsp; |&nbsp;
-    <a href="/intransition/copyright">Copyright/Fair Use Policy</a>
-  </p>
+    </a></li><li>
+    <a href="/intransition/copyright">Copyright/Fair Use Policy</a></li>
+  </ul></div>
   <?php print $content; ?>
-</div>
--- templates/block--footer.tpl.php
+++ templates/block--footer.tpl.php
@@ -1,7 +1,3 @@
 <?php $mclink = $GLOBALS['base_root'] ;  ?>
-<p class="extras">
-	<span><a href="<?php echo $mclink; ?>/about">About MediaCommons</a></span>
-	<span><a href="<?php echo $mclink; ?>/editors">Editors</a></span>
-	<span><a href="<?php echo $mclink; ?>/copyright">Copyright/Fair Use Policy</a></span>
-</p>
+<nav class="footer-nav"><ul><li class="first"><a href="<?php echo $mclink; ?>/about">About MediaCommons</a></li><li><a href="<?php echo $mclink; ?>/editors">Editors</a></li><li><a href="<?php echo $mclink; ?>/copyright">Copyright/Fair Use Policy</a></li></ul></nav>
 <?php print $content; ?>

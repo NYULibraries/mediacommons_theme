cb8f628e124dc2010b05f000382b961db2706178
Laura Henze <laura.henze@nyu.edu>
2018-09-13 11:48:29 -0400

    InTransition footer -- fixing outdated links

--- templates/block--footer--int.tpl.php
+++ templates/block--footer--int.tpl.php
@@ -1,4 +1,4 @@
-<?php $mclink = $GLOBALS['base_root'] . "/mediacommons"; 
+<?php
 $theme_path = $GLOBALS['base_path']  . drupal_get_path('theme', variable_get('theme_default', NULL));
 $main_menu = menu_navigation_links('main-menu');
 ?>
@@ -10,9 +10,9 @@ $main_menu = menu_navigation_links('main-menu');
 <div class="footer-nav">
   <ul><li>
     <strong>[in]Transition:</strong> A
-    <a href="<?php echo $mclink; ?>">MediaCommons</a>/
-    <a href="http://www.cmstudies.org/?page=cinema_journal">Cinema Journal</a> Project</li><li>
-    <a href="http://www.cmstudies.org/" target="_blank" title="Society For Cinema and Media Studies" style="line-height: 1;font-size: .1rem; display: inline-block; vertical-align: bottom;">
+    <a href="/">MediaCommons</a> /
+    <a href="https://www.cmstudies.org/?page=cinema_journal"  target="_blank">Cinema Journal</a> Project</li><li>
+    <a href="https://www.cmstudies.org" target="_blank" title="Society For Cinema and Media Studies" style="line-height: 1;font-size: .1rem; display: inline-block; vertical-align: bottom;">
       <img alt="Society For Cinema and Media Studies" src="<?php echo $theme_path;?>/images/logos/scms_logo_sm.gif">
     </a></li><li>
     <a href="/intransition/copyright">Copyright/Fair Use Policy</a></li>
